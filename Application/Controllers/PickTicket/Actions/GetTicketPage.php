86e0308b6a79054d1ad67f0d75155a5fd3c73a7f
Alex <aagarciga@gmail.com>
2014-10-01 23:10:11 -0400

    GetTicketPage Action Method

--- Application/Controllers/PickTicket/Actions/GetTicketPage.php
+++ Application/Controllers/PickTicket/Actions/GetTicketPage.php
@@ -20,15 +20,27 @@ class GetTicketPage extends Action {
      * @return JSON
      */
     public function Execute() {
-        $pono = filter_input(INPUT_GET, 'page');
-                
-        $result = array();        
+        $page = filter_input(INPUT_GET, 'page');
         
-        if ($successful) {
-            $result['success'] = true;
-        }
-        else{
-            $result['success'] = false;
+        $result = array();
+        
+        if (is_numeric($page)) {
+            $this->Pager = $this->controller->DatUnitOfWork->SOSHPRELRepository->GetTicketsPager(10);
+            $this->Pager->ajaxPaginate($page);
+            $queryResult = $this->Pager->getCurrentPagedItems();
+            foreach ($queryResult as $row) {
+                $current = array();
+                $current['company'] = $this->controller->DatUnitOfWork->SOSHPRELHRepository->GetTicketCompanyByOrdNum(trim($row->ORDNUM));
+                $current['shprelno'] = $row->SHPRELNO;
+                $current['ordnum'] = $row->ORDNUM;
+                $current['shpreldate'] = $row->SHPRELDATE;
+                $current['bath_no'] = $row->BATCH_NO;
+                $current['qtyshprel'] = $row->QTYSHPREL;
+                $current['qtypick'] = $row->QTYPICK;
+                $current['qtypack'] = $row->QTYPACK;
+                $current['weight'] = $row->WEIGHT;
+                $result[] = $current;
+            }         
         }
         
         return json_encode($result);
