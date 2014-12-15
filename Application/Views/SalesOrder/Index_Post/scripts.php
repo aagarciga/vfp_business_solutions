f1845533a690b71498e0123b43d81e77dde52dfb
Alex <aagarciga@gmail.com>
2014-12-14 23:06:25 -0500

    Javascript Module Pattern Added to Sales Order script.js file

--- Application/Views/SalesOrder/Index_Post/scripts.php
+++ Application/Views/SalesOrder/Index_Post/scripts.php
@@ -1,2 +1,35 @@
 <script>
+    Dandelion.namespace('App.Screen.SalesOrder', window);
+    
+    // Module Pattern
+    window.App.Screen.SalesOrder = (function (){
+        // Dependencies
+//        var objectA = App.Classes.ClassA,
+//            objectB = App.Classes.ClassB;
+        
+        // Private Properties (without var)
+        emptyArray = [],
+        string1 = "String",
+        int1 = 7,
+        objectString = Object.prototype.toString();
+        
+        // Private Methods 
+        privateMethod1 = function(){
+            // TODO: Private Method1 Code goes here...
+        };
+        // End VAR
+        
+        // Optionally one-time init procedures
+        init = function(){
+            // TODO: Private Method1 Code goes here...
+        };
+        
+        return {
+            publicMethod1 : function(){
+                // TODO: Public Method1 Code goes here...
+            }
+        };
+        
+        this.init();
+    })();
 </script>
