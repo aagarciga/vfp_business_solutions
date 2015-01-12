730494e8b9e33272663cd3fd2d30ffc30e2c6d5a
Alex <aagarciga@gmail.com>
2015-01-12 18:57:48 -0500

    Revert "Revert "Working on Dandelion MVC Creator""
    This reverts commit 513f71d101bebc5efecfc535fc364479e40a126e.

--- Application/Controllers/Creator/Actions/Controllers.php
+++ Application/Controllers/Creator/Actions/Controllers.php
@@ -24,7 +24,7 @@ use Dandelion\MVC\Core\Action;
 /**
  * 
  */
-class Index extends Action {
+class Controllers extends Action {
 
     public function Execute() {
         $this->Title = 'Creator | Dandelion MVC Solutions';
