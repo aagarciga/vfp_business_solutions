07c00941b8b3a03f667d1ee6756b70a533e30727
Alex <aagarciga@gmail.com>
2015-01-23 18:21:25 -0500

    Removed Edit Item in jsTree context menu

--- Application/Views/Dashboard/Index/scripts.php
+++ Application/Views/Dashboard/Index/scripts.php
@@ -593,21 +593,8 @@
                 contextmenu: {
                     items: function (node) {
                         var tmp = $.jstree.defaults.contextmenu.items();
-                        delete tmp.create.action;
-                        tmp.create.label = "New";
-                        tmp.create.submenu = {
-                            "create_folder" : {
-                                "separator_after" : true,
-                                "label"	: "Folder",
-                                "action" : function (data) {
-                                    var inst = $.jstree.reference(data.reference),
-                                        obj = inst.get_node(data.reference);
-                                    inst.create_node(obj, { type : "default" }, "last", function (new_node) {
-                                        setTimeout(function () { inst.edit(new_node); },0);
-                                    });
-                                }
-                            }
-                        };
+                        // Removing Edit Options
+                        delete tmp.ccp;
                         if(this.get_type(node) === "file") {
                             delete tmp.create;
                         }
