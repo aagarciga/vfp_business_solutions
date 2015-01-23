7ea145c027b9f4ac86cc144678a0a103c0d09116
Alex <aagarciga@gmail.com>
2015-01-23 03:04:13 -0500

    Tree contextMenu behavior almost done.

--- Application/Views/Dashboard/Index/scripts.php
+++ Application/Views/Dashboard/Index/scripts.php
@@ -523,184 +523,187 @@
         var app = global.App,
         dashboard = app.Dashboard;
         dashboard.currentProject = {'salesorder' : ''};
+        dashboard.filesModal = { id : '#files-modal'};
+        dashboard.filesModal.controls = [];
+        dashboard.filesModal.controls['jstree'] = {
+            id : '#jstree'
+        };
+        
+        dashboard.$filesModal = $(dashboard.filesModal.id);
         
         dashboard.projectAttachButton_OnClick = function (event) {            
             dashboard.currentProject.salesorder = $(event.currentTarget).parent().parent().find('.item-field a').html();
 //            dashboard.jsTreeInstance.init(dashboard.currentProject);
-            cosa();
+            dashboard.filesModal.loadProjectTree(dashboard.currentProject);
 
-            $('#files-modal').modal('show');
+            dashboard.$filesModal.modal('show');
         };
         
-        var cosa = function (){
-        $('#jstree')
-        .jstree({
-            'core': {
-                'data': {
-                    'url': 'index.php?controller=dashboard&action=projectattachementsapi&salesorder=10005&operation=get_node',
-                    'data': function (node) {
-                        return {'id': node.id};
+        dashboard.filesModal.loadProjectTree = function (currentProject) {
+            console.log(currentProject.salesorder);         
+            
+            // TODO: Create jsTree Instance
+            $jstree = $(dashboard.filesModal.controls['jstree'].id);            
+            
+            // Reset jsTree Instance
+            if (dashboard.filesModal.controls['jstree'].instance) {
+                dashboard.filesModal.controls['jstree'].instance.jstree('destroy');
+            }
+            dashboard.filesModal.controls['jstree'].instance = $jstree.jstree({
+                plugins : ['state','dnd','sort','types','contextmenu','unique'],
+                core: {
+                    data: {
+                        url: 'index.php?controller=dashboard&action=projectattachementsapi&salesorder='+currentProject.salesorder+'&operation=get_node',
+                        data: function (node) {
+                            return { id: node.id };
+                        }
+                    },
+                    themes: {
+                        name: 'default',
+                        responsive : false,
+                        variant : 'medium',
+                        stripes : false
+                    },
+                    check_callback : function(operation, node, node_parent, node_position, more) {
+                        if(more && more.dnd && more.pos !== 'i') { return false; } // If error change 'i' for node_position
+                        if(operation === "move_node" || operation === "copy_node") {
+                            if(this.get_node(node).parent === this.get_node(node_parent).id) { return false; }
+                        }
+                        return true;
+                    },
+                    animation: true
+                },
+                sort: function(a, b) {
+                    return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
+                },
+                types: {
+                    '#': {
+                        'max_children': 1,
+                        valid_children: ['default'],
+                        icon: 'glyphicon glyphicon-folder-close'
+                    },
+                    'default': {
+                        valid_children: ['default'],
+                        icon: 'glyphicon glyphicon-folder-close'
+                    } 
+                },
+                contextmenu: {
+                    items: function (node) {
+                        var tmp = $.jstree.defaults.contextmenu.items();
+                        delete tmp.create.action;
+                        tmp.create.label = "New";
+                        tmp.create.submenu = {
+                            "create_folder" : {
+                                "separator_after" : true,
+                                "label"	: "Folder",
+                                "action" : function (data) {
+                                    var inst = $.jstree.reference(data.reference),
+                                            obj = inst.get_node(data.reference);
+                                    inst.create_node(obj, { type : "default" }, "last", function (new_node) {
+                                            setTimeout(function () { inst.edit(new_node); },0);
+                                    });
+                                }
+                            }
+                        };
+                        if(this.get_type(node) === "file") {
+                            delete tmp.create;
+                        }
+                        return tmp;
                     }
                 },
-                'check_callback': function (o, n, p, i, m) {
-                    if (m && m.dnd && m.pos !== 'i') {
-                        return false;
+                unique : {
+                    duplicate : function (name, counter) {
+                        return name + ' ' + counter;
                     }
-                    if (o === "move_node" || o === "copy_node") {
-                        if (this.get_node(n).parent === this.get_node(p).id) {
-                            return false;
-                        }
-                    }
-                    return true;
-                },
-                'animation': true,
-                'themes': {
-                    'name': 'default'
-                            //'variant' : 'large',
-                            //'stripes' : false
                 }
-            },
-            'sort': function (a, b) {
-                return this.get_type(a) === this.get_type(b) ? (this.get_text(a) > this.get_text(b) ? 1 : -1) : (this.get_type(a) >= this.get_type(b) ? 1 : -1);
-            },
-            'contextmenu': {
-                'items': function (node) {
-                    var tmp = $.jstree.defaults.contextmenu.items();
-                    delete tmp.create.action;
-                    tmp.create.label = "New";
-                    tmp.create.submenu = {
-                        "create_folder": {
-                            "separator_after": true,
-                            "label": "Folder",
-                            "action": function (data) {
-                                var inst = $.jstree.reference(data.reference),
-                                        obj = inst.get_node(data.reference);
-                                inst.create_node(obj, {type: "default"}, "last", function (new_node) {
-                                    setTimeout(function () {
-                                        inst.edit(new_node);
-                                    }, 0);
-                                });
-                            }
-                        },
-                        "create_file": {
-                            "label": "File",
-                            "action": function (data) {
-                                var inst = $.jstree.reference(data.reference),
-                                        obj = inst.get_node(data.reference);
-                                inst.create_node(obj, {type: "file"}, "last", function (new_node) {
-                                    setTimeout(function () {
-                                        inst.edit(new_node);
-                                    }, 0);
-                                });
-                            }
-                        }
-                    };
-                    if (this.get_type(node) === "file") {
-                        delete tmp.create;
-                    }
-                    return tmp;
-                }
-            },
-            'types': {
-                '#': {'max_children': 1, 'valid_children': ['default'], 'icon': 'glyphicon glyphicon-folder-close'},
-                'default': {'icon': 'glyphicon glyphicon-folder-close'},
-                'file': {'valid_children': [], 'icon': 'glyphicon glyphicon-folder-close'}
-            },
-            'unique': {
-                'duplicate': function (name, counter) {
-                    return name + ' ' + counter;
-                }
-            },
-            'plugins': ['state', 'dnd', 'sort', 'types', 'contextmenu', 'unique']
-        })
-        .on('delete_node.jstree', function (e, data) {
-            $.get('?operation=delete_node', {'id': data.node.id})
-                    .fail(function () {
-                        data.instance.refresh();
-                    });
-        })
-        .on('create_node.jstree', function (e, data) {
-            $.get('?operation=create_node', {'type': data.node.type, 'id': data.node.parent, 'text': data.node.text})
-                    .done(function (d) {
-                        data.instance.set_id(data.node, d.id);
-                    })
-                    .fail(function () {
-                        data.instance.refresh();
-                    });
-        })
-        .on('rename_node.jstree', function (e, data) {
-            $.get('?operation=rename_node', {'id': data.node.id, 'text': data.text})
-                    .done(function (d) {
-                        data.instance.set_id(data.node, d.id);
-                    })
-                    .fail(function () {
-                        data.instance.refresh();
-                    });
-        })
-        .on('move_node.jstree', function (e, data) {
-            $.get('?operation=move_node', {'id': data.node.id, 'parent': data.parent})
-                    .done(function (d) {
-                        //data.instance.load_node(data.parent);
-                        data.instance.refresh();
-                    })
-                    .fail(function () {
-                        data.instance.refresh();
-                    });
-        })
-        .on('copy_node.jstree', function (e, data) {
-            $.get('?operation=copy_node', {'id': data.original.id, 'parent': data.parent})
-                    .done(function (d) {
-                        //data.instance.load_node(data.parent);
-                        data.instance.refresh();
-                    })
-                    .fail(function () {
-                        data.instance.refresh();
-                    });
-        })
-        .on('changed.jstree', function (e, data) {
-            if (data && data.selected && data.selected.length) {
-                $.get('?operation=get_content&id=' + data.selected.join(':'), function (d) {
-                    if (d && typeof d.type !== 'undefined') {
-                        $('#data .content').hide();
-                        switch (d.type) {
-                            case 'text':
-                            case 'txt':
-                            case 'md':
-                            case 'htaccess':
-                            case 'log':
-                            case 'sql':
-                            case 'php':
-                            case 'js':
-                            case 'json':
-                            case 'css':
-                            case 'html':
-                                $('#data .code').show();
-                                $('#code').val(d.content);
-                                break;
-                            case 'png':
-                            case 'jpg':
-                            case 'jpeg':
-                            case 'bmp':
-                            case 'gif':
-                                $('#data .image img').one('load', function () {
-                                    $(this).css({'marginTop': '-' + $(this).height() / 2 + 'px', 'marginLeft': '-' + $(this).width() / 2 + 'px'});
-                                }).attr('src', d.content);
-                                $('#data .image').show();
-                                break;
-                            default:
-                                $('#data .default').html(d.content).show();
-                                break;
-                        }
-                    }
+            })
+            .on('delete_node.jstree', function (e, data) {
+                $.get('index.php?controller=dashboard&action=projectattachementsapi&salesorder='+currentProject.salesorder+'&operation=delete_node', { 'id' : data.node.id })
+                .fail(function () {
+                    data.instance.refresh();
                 });
-            }
-            else {
-                $('#data .content').hide();
-                $('#data .default').html('Select a file from the tree.').show();
-            }
+            })
+            .on('create_node.jstree', function (e, data) {
+                $.get('index.php?controller=dashboard&action=projectattachementsapi&salesorder='+currentProject.salesorder+'&operation=create_node', { 'type' : data.node.type, 'id' : data.node.parent, 'text' : data.node.text })
+                .done(function (d) {
+                    data.instance.set_id(data.node, d.id);
+                })
+                .fail(function () {
+                    data.instance.refresh();
+                });
+            })
+            .on('rename_node.jstree', function (e, data) {
+                $.get('index.php?controller=dashboard&action=projectattachementsapi&salesorder='+currentProject.salesorder+'&operation=rename_node', { 'id' : data.node.id, 'text' : data.text })
+                .done(function (d) {
+                    data.instance.set_id(data.node, d.id);
+                })
+                .fail(function () {
+                    data.instance.refresh();
+                });
+            })
+            .on('move_node.jstree', function (e, data) {
+                $.get('index.php?controller=dashboard&action=projectattachementsapi&salesorder='+currentProject.salesorder+'&operation=move_node', { 'id' : data.node.id, 'parent' : data.parent })
+                .done(function (d) {
+                    //data.instance.load_node(data.parent);
+                    data.instance.refresh();
+                })
+                .fail(function () {
+                    data.instance.refresh();
+                });
+            })
+            .on('copy_node.jstree', function (e, data) {
+                $.get('index.php?controller=dashboard&action=projectattachementsapi&salesorder='+currentProject.salesorder+'&operation=copy_node', { 'id' : data.original.id, 'parent' : data.parent })
+                    .done(function (d) {
+                        data.instance.load_node(data.parent);
+                        data.instance.refresh();
+                    })
+                    .fail(function () {
+                        data.instance.refresh();
+                    });
+            })
+            .on('changed.jstree', function (e, data) {
+                console.log('event', e);
+                console.log('data', data);
+//                if(data && data.selected && data.selected.length) {
+//                    $.get('?operation=get_content&id=' + data.selected.join(':'), function (d) {
+//                        if(d && typeof d.type !== 'undefined') {
+//                            $('#data .content').hide();
+//                            switch(d.type) {
+//                                case 'text':
+//                                case 'txt':
+//                                case 'md':
+//                                case 'htaccess':
+//                                case 'log':
+//                                case 'sql':
+//                                case 'php':
+//                                case 'js':
+//                                case 'json':
+//                                case 'css':
+//                                case 'html':
+//                                    $('#data .code').show();
+//                                    $('#code').val(d.content);
+//                                    break;
+//                                case 'png':
+//                                case 'jpg':
+//                                case 'jpeg':
+//                                case 'bmp':
+//                                case 'gif':
+//                                    $('#data .image img').one('load', function () { $(this).css({'marginTop':'-' + $(this).height()/2 + 'px','marginLeft':'-' + $(this).width()/2 + 'px'}); }).attr('src',d.content);
+//                                    $('#data .image').show();
+//                                    break;
+//                                default:
+//                                    $('#data .default').html(d.content).show();
+//                                    break;
+//                            }
+//                        }
+//                    });
+//                }
+//                else {
+//                    $('#data .content').hide();
+//                    $('#data .default').html('Select a file from the tree.').show();
+//                }
         });
-        }
-        
+        };
         
         $('.btn-files-dialog').on('click', dashboard.projectAttachButton_OnClick);
     }(window, window.dandelion));
