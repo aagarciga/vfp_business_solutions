f0e7b0adae57aa377c994ce1f313dd59d76d88fd
Alex <aagarciga@gmail.com>
2015-06-19 16:48:45 -0400

    issue:vfpbs-9. Show quote header data.
    subtask:vfpbs-18. Do a refactoring on main Menu.

--- Application/Views/QuoteDashboard/Index/body.php
+++ Application/Views/QuoteDashboard/Index/body.php
@@ -15,25 +15,7 @@
             </a>
         </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <?php echo $UserName ?> <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <?php if ($FullFeatures === true):?>
-                        <li><a href="<?php echo $View->Href("Main", "Index") ?>" class="main active">
-                                <span class="glyphicon glyphicon-home"></span> Main</a></li>
-                        <?php endif ?>
-                        <li><a href="<?php echo $View->Href("Dashboard", "Index") ?>" class="exit">
-                                <span class="glyphicon glyphicon-th"></span> Dashboard</a></li>
-                        <li><a href="<?php echo $View->Href("User", "Signout") ?>" class="exit">
-                                <span class="glyphicon glyphicon-off"></span> Exit</a></li>
-
-                    </ul>
-                </li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
+        <?php $View->Control('MainMenu'); ?>
     </nav>
 
     <div class="panel panel-default">
