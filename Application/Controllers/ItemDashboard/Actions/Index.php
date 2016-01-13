dc63e6a1b7ba76b36935d6c3340e7f18bd463208
Alex Alvarez Gárciga <aagarciga@gmail.com>
2016-01-13 14:36:36 -0500

    Error de ARCOMPRepository corregido, en ItemDashboard/Index action.

--- Application/Controllers/ItemDashboard/Actions/Index.php
+++ Application/Controllers/ItemDashboard/Actions/Index.php
@@ -8,8 +8,41 @@
 
 namespace Dandelion\MVC\Application\Controllers\ItemDashboard\Actions;
 
+use Dandelion\MVC\Application\Controllers\ItemDashboard\Models\ItemDashboardViewModel;
+use Dandelion\MVC\Core\Action;
 
-class Index
+class Index extends Action
 {
+    public function Execute()
+    {
+        $itemno = $this->Request->hasProperty('itemno') ? $this->Request->itemno : '';
 
+        $this->Title = 'Item Dashboard | VFP Business Series';
+
+        $defaultItemsPerPage = $this->Request->Application->getDefaultPagerItermsPerPage();
+
+        $this->UserName = (!isset($_SESSION['username'])) ? 'Anonimous' : $_SESSION['username'];
+        $this->ItemPerPage = isset($_SESSION['itemperpages']) ? $_SESSION['itemperpages'] : $defaultItemsPerPage;
+
+        $this->Itemno = $itemno;
+
+        $this->FilterPredicate = "";
+
+        $this->Pager = $this->controller->GetPager($this->FilterPredicate, $this->ItemPerPage);
+        $this->Pager->Paginate();
+        $items = $this->Pager->getCurrentPagedItems();
+        $viewModels = array();
+
+        foreach($items as $item)
+        {
+            $currentItemViewModel = new ItemDashboardViewModel($item->ordnum, $item->ponum, $item->custno, $item->podate, $item->qtyord, $item->qtyshp, $item->bckord, $item->qtyshp0, $item->qtyshprel, $item->shipdate);
+            $viewModels[] = $currentItemViewModel;
+        }
+
+        $this->Items = $viewModels;
+
+        $this->CompanyLogo = $this->controller->DatUnitOfWork->ARCOMPRepository->GetCompanyLogo();
+
+        $this->FullFeatures = isset($_SESSION['fullFeatures']) ? $_SESSION['fullFeatures'] : false;
+    }
 }
