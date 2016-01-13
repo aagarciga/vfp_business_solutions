85264b391b686768644e29b82fd61858125ce9c5
Alex Alvarez Gárciga <aagarciga@gmail.com>
2016-01-13 01:09:59 -0500

    Action Index Execute version 1.0

---
+++ Application/Controllers/ItemDashboard/Actions/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 13/01/2016
+ * Time: 0:34
+ */
+
+namespace Dandelion\MVC\Application\Controllers\ItemDashboard\Actions;
+
+
+class Index
+{
+
+}
