cef4a779918f5217a734ba28b97917004d22cf6c
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-01-27 19:43:15 +0100

    Created createArrayModel method.
    Created createFixMethod method.

--- Application/Tools/Tools.php
+++ Application/Tools/Tools.php
@@ -11,6 +11,12 @@ namespace Dandelion\MVC\Application\Tools;
 
 define("DATE_NULL_VALUE", '1899-12-30');
 
+define("FIX_PREFIX", 'fix_');
+
+define("FIX_DEFAULT", 'default');
+
+define("SET_PREFIX", 'set');
+
 /**
  * @param string $viewModelClass type of return view model
  * @param object $item db item
@@ -23,10 +29,46 @@ function createViewModel($viewModelClass, $item, $fieldDefinition){
         $arrayField = str_split($field);
         $arrayField[0] = strtoupper($arrayField[0]);
         $suffix = implode($arrayField);
-        $setMethod = 'set' . $suffix;
-        $fixMethod = 'fix_' . $type;
+        $setMethod = SET_PREFIX . $suffix;
+        $fixMethod = createFixMethod($type);
         $value = $fixMethod($item->$field);
         $result.$setMethod($value);
     }
     return $result;
+}
+
+/**
+ * @param string $type the type of field
+ * @return string name of fix method
+ */
+function createFixMethod($type)
+{
+    $fixMethod = FIX_PREFIX . $type;
+
+    if (!function_exists($fixMethod)) {
+        $fixMethod = FIX_PREFIX . FIX_DEFAULT;
+    }
+    return $fixMethod;
+}
+
+/**
+ * @param object $item db item
+ * @param array $fieldDefinition definition of the field "field => type"
+ * @return array object with field set
+ */
+function createArrayModel($item, $fieldDefinition){
+    $result = array();
+    foreach ($fieldDefinition as $field => $type){
+        $fixMethod = createFixMethod($type);
+        $value = $fixMethod($item->$field);
+        $result[$field] = $value;
+    }
+    return $result;
+}
+
+function fix_default($value){
+    if (is_string($value)){
+        return trim($value);
+    }
+    return $value;
 }
