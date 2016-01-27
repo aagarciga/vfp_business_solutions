c9050883afe596f1f9b40943cd3013d642ab7f3e
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-01-26 21:13:30 -0500

    Created EquipmentDashboard controller and action.
    Created EquipmentDashboard index view.
    Created EquipmentDashboard view-model.
    Created EquipmentDashboard scripts.
    Created EquipmentDashboard styles.

---
+++ Application/Controllers/EquipmentDashboard/Actions/GetPage_Post.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * User: Victor
+ * Date: 04/12/2016
+ * Time: 8:15
+ * Project:  VFP Business Series
+ * Copyright: 2014. VFP Business Solutions, LLC
+ */
+
+namespace Dandelion\MVC\Application\Controllers\EquipmentDashboard\Actions;
+
+use Dandelion\MVC\Core\Action;
+
+/**
+ * Created by: Victor
+ * Class GetPage_Post
+ * @package Dandelion\MVC\Application\Controllers\EquipmentDashboard\Actions
+ */
+class GetPage_Post extends Action
+{
+    public function Execute()
+    {
+        $this->Itemno = $itemno = $this->Request->hasProperty('itemno') ? base64_decode($this->Request->itemno) : '';
+        $this->Itemwhs = $itemwhs = $this->Request->hasProperty('itemwhs') ? base64_decode($this->Request->itemwhs) : '';
+        $userFilterPredicate = $this->Request->hasProperty('predicate') ? $this->Request->predicate : "";
+
+        //todo: Set default value as global default value
+        $page = $this->Request->hasProperty('page') ? $this->Request->page : 1;
+        $itemsPerPage = $this->Request->hasProperty('itemsPerPage') ? $this->Request->itemsPerPage : 50;
+        $orderby = $this->Request->hasProperty('orderby') ? $this->Request->orderby : "pono";
+        $order = $this->Request->hasProperty('order') ? $this->Request->order : "ASC";
+
+        $defaultPredicate = $this->controller->GetOnOrderPredicate($itemno, $itemwhs);
+
+        $key = $itemno.$itemwhs;
+
+        if ($key !== "" && $userFilterPredicate !== "")
+        {
+            $filterPredicate = "$userFilterPredicate AND $defaultPredicate";
+        }
+        elseif ($key !== "")
+        {
+            $filterPredicate = $defaultPredicate;
+        }
+
+        $this->FilterPredicate = $_SESSION['EquipmentDashboard_filterPredicate'] = $userFilterPredicate;
+        $this->ItemPerPage = $_SESSION['EquipmentDashboard_itemperpages'] = $itemsPerPage;
+
+        $result = array();
+
+        $this->Pager = $this->controller->GetPager($filterPredicate, $this->ItemPerPage, 5, 10, $orderby, $order);
+        $pager = $this->Pager->PaginateForAjax($page);
+        $itemCount = $this->Pager->getItemsCount();
+        $queryResult = $this->Pager->getCurrentPagedItems();
+
+        if ($itemCount > 0)
+        {
+            foreach ($queryResult as $item) {
+                $poTypeValue = trim($item->podate);
+                $shippedValue = trim($item->shipped);
+                $current = array();
+                $current['pono'] = trim($item->pono);
+                $current['vendno'] = trim($item->vendno);
+                $current['podate'] = ($poTypeValue === DATE_NULL_VALUE) ? '' : $poTypeValue;
+                $current['qtyord'] = trim($item->qtyord);
+                $current['qtyrec'] = trim($item->qtyrec);
+                $current['qtyleft'] = trim($item->qtyleft);
+                $current['shipped'] = ($shippedValue === DATE_NULL_VALUE) ? '' : $shippedValue;
+                $current['potype'] = trim($item->potype);
+                $result[] = $current;
+            }
+        }
+
+        $pager['currentPagedItems'] = $result;
+
+        return json_encode($pager);
+    }
+
+}
---
+++ Application/Controllers/EquipmentDashboard/Actions/Index.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victor
+ * Date: 13/01/2016
+ * Time: 0:34
+ * Project:  VFP Business Series
+ * Copyright: 2014. VFP Business Solutions, LLC
+ */
+
+namespace Dandelion\MVC\Application\Controllers\EquipmentDashboard\Actions;
+
+use Dandelion\MVC\Application\Controllers\EquipmentDashboard\Models\EquipmentDashboardViewModel;
+use Dandelion\MVC\Core\Action;
+use Dandelion\Diana\BootstrapPager;
+
+/**
+ * Created by: Victor
+ * Class Index
+ * @package Dandelion\MVC\Application\Controllers\EquipmentDashboard\Actions
+ */
+class Index extends Action
+{
+    public function Execute()
+    {
+        $exportedBy = 'OPO';
+        $this->Itemno = $itemno = $this->Request->hasProperty('itemno') ? base64_decode($this->Request->itemno) : '';
+        $this->Itemwhs = $itemwhs = $this->Request->hasProperty('itemwhs') ? base64_decode($this->Request->itemwhs) : '';
+
+        $this->Title = 'On Purchase Order Dashboard | VFP Business Series';
+
+        $defaultItemsPerPage = $this->Request->Application->getDefaultPagerItermsPerPage();
+
+        $this->UserName = (!isset($_SESSION['username'])) ? 'Anonimous' : $_SESSION['username'];
+        $this->ItemPerPage = isset($_SESSION['itemperpages']) ? $_SESSION['itemperpages'] : $defaultItemsPerPage;
+
+        $this->FilterPredicate = $this->controller->GetOnOrderPredicate($itemno, $itemwhs);
+
+        $this->Pager = $this->controller->GetPager($this->FilterPredicate, $this->ItemPerPage);
+        $this->Pager->Paginate();
+        $itemCount = $this->Pager->getItemsCount();
+        $items = $this->Pager->getCurrentPagedItems();
+        $viewModels = array();
+
+        if ($itemCount > 0)
+        {
+            foreach($items as $item)
+            {
+                $poTypeValue = trim($item->podate);
+                $shippedValue = trim($item->shipped);
+                $poTypeValue = ($poTypeValue === DATE_NULL_VALUE) ? '' : $poTypeValue;
+                $shippedValue = ($shippedValue === DATE_NULL_VALUE) ? '' : $shippedValue;
+                $currentItemViewModel = new EquipmentDashboardViewModel($item->pono, $item->vendno, $poTypeValue,
+                    $item->qtyord, $item->qtyrec, $item->qtyleft, $shippedValue, $item->potype);
+                $viewModels[] = $currentItemViewModel;
+            }
+        }
+
+        $this->Items = $viewModels;
+        $user = $this->controller->VfpDataUnitOfWork->SysuserRepository->GetByUsername($this->UserName);
+        $this->SavedUserFilters = $this->controller->VfpDataUnitOfWork->SysexportRepository->GetSavedFiltersByUserName($user->getUsername(), $exportedBy);
+
+        $this->CompanyLogo = $this->controller->DatUnitOfWork->ARCOMPRepository->GetCompanyLogo();
+
+        $this->FullFeatures = isset($_SESSION['fullFeatures']) ? $_SESSION['fullFeatures'] : false;
+        $this->ShowFiancialDashboard = (!isset($_SESSION['showFiancialDashboard'])) ? false : $_SESSION['showFiancialDashboard'];
+
+        $this->JavascriptBootstrapPager = BootstrapPager::GetJavascriptBootstrapPager();
+    }
+}
