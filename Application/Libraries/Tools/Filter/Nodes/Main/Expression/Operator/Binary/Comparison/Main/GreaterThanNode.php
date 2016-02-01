41468a81d7c50f5db03e8377b5c539d50e4ada1a
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-02-01 14:45:09 -0500

    Created EqualNode class.
    Created GreaterEqualThanNode class.
    Created GreaterThanNode class.
    Created LessEqualThanNode class.
    Created LessThanNode class.
    Created NotEqualNode class.

---
+++ Application/Libraries/Tools/Filter/Nodes/Main/Expression/Operator/Binary/Comparison/Main/GreaterThanNode.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: Victor
+ * Date: 01/02/2016
+ * Time: 14:35
+ * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
+ * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.thedandelionproject.com
+ */
+
+namespace Dandelion\Tools\Filter;
+
+use Dandelion\Tools\Filter\ComparisonBinaryOperatorNode;
+
+class GreaterThanNode extends ComparisonBinaryOperatorNode
+{
+    public function checkSemantic($report)
+    {
+        // TODO: Implement checkSemantic() method.
+    }
+
+    public function generateSqlCode($codeGenerator)
+    {
+        // TODO: Implement generateSqlCode() method.
+    }
+
+    public function generateHtmlCode($codeGenerator)
+    {
+        // TODO: Implement generateHtmlCode() method.
+    }
+
+}
