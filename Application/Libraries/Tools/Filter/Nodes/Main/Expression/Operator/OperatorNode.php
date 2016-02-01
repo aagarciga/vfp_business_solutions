60d2bcfdf06706c4918547080eac14980b096b7a
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-02-01 13:55:30 -0500

    Created BinaryOperatorNode class.
    Created OperatorNode class.
    Created UnaryOperator class.

---
+++ Application/Libraries/Tools/Filter/Nodes/Main/Expression/Operator/OperatorNode.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Victor
+ * Date: 01/02/2016
+ * Time: 13:38
+ * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
+ * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.thedandelionproject.com
+ */
+
+namespace Dandelion\Tools\Filter;
+
+use Dandelion\Tools\Filter\ExpressionNode;
+
+abstract class OperatorNode extends ExpressionNode
+{
+
+}
