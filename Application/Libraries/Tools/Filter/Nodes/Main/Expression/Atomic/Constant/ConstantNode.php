337bf0c567ac6efc4c6e0510055414d3045de13c
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-02-01 19:41:04 -0500

    Created AtomicNode class.
    Created ConstantNode class.
    Created DateNode class.
    Created FloatNode class.
    Created IntNode class.
    Created StringNode class.
    Created FieldNode class.
    Created NotNode class.

--- Application/Libraries/Tools/Filter/Nodes/Main/Expression/Atomic/Constant/ConstantNode.php
+++ Application/Libraries/Tools/Filter/Nodes/Main/Expression/Atomic/Constant/ConstantNode.php
@@ -2,7 +2,7 @@
 /**
  * User: Victor
  * Date: 01/02/2016
- * Time: 11:05
+ * Time: 19:29
  * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
  * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
@@ -11,9 +11,9 @@
 
 namespace Dandelion\Tools\Filter;
 
-use Dandelion\Tools\Filter\ExpressionNode;
+use Dandelion\Tools\Filter\AtomicNode;
 
-abstract class ValueNode extends ExpressionNode
+abstract class ConstantNode extends AtomicNode
 {
 
 }
---
+++ Application/Libraries/Tools/Filter/Nodes/Main/Expression/Atomic/Field/FieldNode.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: Victor
+ * Date: 01/02/2016
+ * Time: 19:37
+ * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
+ * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.thedandelionproject.com
+ */
+
+namespace Dandelion\Tools\Filter;
+
+use Dandelion\Tools\Filter\ConstantNode;
+
+class FieldNode extends ConstantNode
+{
+    public function checkSemantic($report)
+    {
+        // TODO: Implement checkSemantic() method.
+    }
+
+    public function generateSqlCode($codeGenerator)
+    {
+        // TODO: Implement generateSqlCode() method.
+    }
+
+    public function generateHtmlCode($codeGenerator)
+    {
+        // TODO: Implement generateHtmlCode() method.
+    }
+
+}
