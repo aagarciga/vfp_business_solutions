b47c1fd2f06aee93de13a75eb5e3e0a5d136057c
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-02-01 20:57:28 -0500

    Created BaseCodeGenerator class.
    Created HtmlGenerator class.
    Created HtmlBlockTagGenerator class.
    Created HtmlTagsGenerator class.
    Created SqlPredicateGenerator class.

---
+++ Application/Libraries/Tools/CodeGenerator/Generator/BaseCodeGenerator.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: Victor
+ * Date: 01/02/2016
+ * Time: 20:40
+ * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
+ * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.thedandelionproject.com
+ */
+
+namespace Dandelion\Tools\CodeGenerator;
+
+use Dandelion\Tools\CodeGenerator\ICodeGenerator;
+
+abstract class BaseCodeGenerator implements ICodeGenerator
+{
+    abstract function getCode();
+
+    abstract function InsertCode($code);
+}
---
+++ Application/Libraries/Tools/CodeGenerator/Generator/HtmlGenerator/Main/HtmlBlockTagGenerator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: Victor
+ * Date: 01/02/2016
+ * Time: 20:53
+ * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
+ * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.thedandelionproject.com
+ */
+
+namespace Dandelion\Tools\CodeGenerator;
+
+use Dandelion\Tools\CodeGenerator\HtmlGenerator;
+
+class HtmlBlockTagGenerator extends HtmlGenerator
+{
+    function getCode()
+    {
+        // TODO: Implement getCode() method.
+    }
+
+    function InsertCode($code)
+    {
+        // TODO: Implement InsertCode() method.
+    }
+
+}
