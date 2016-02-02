af9222eab0815213ffd84ded53b3a32535cd5cb6
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-02-01 21:46:22 -0500

    Created IHtmlAttribute interface.
    Add InsertAttribute, getAttributeCount, getAttribute on IVirtualCode interface.

--- Application/Libraries/Tools/CodeGenerator/Interfaces/IHtmlAttribute.php
+++ Application/Libraries/Tools/CodeGenerator/Interfaces/IHtmlAttribute.php
@@ -2,7 +2,7 @@
 /**
  * User: Victor
  * Date: 01/02/2016
- * Time: 19:44
+ * Time: 21:41
  * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
  * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
@@ -11,13 +11,9 @@
 
 namespace Dandelion\Tools\CodeGenerator;
 
-interface IVirtualCode
+interface IHtmlAttribute
 {
-    function getCode();
+    function getAttributeName();
 
-    function InsertAttribute();
-
-    function getAttributeCount();
-
-    function getAttribute($index);
+    function getValue();
 }
