fa68962df40973bc740847160dc1e850266ed8c5
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-02-01 19:49:36 -0500

    Created ICode interface.
    Fixed ICodeGenerator, extends ICode and add method InsertCode.

--- Application/Libraries/Tools/CodeGenerator/Interfaces/ICode.php
+++ Application/Libraries/Tools/CodeGenerator/Interfaces/ICode.php
@@ -1,19 +1,17 @@
 <?php
 /**
  * User: Victor
- * Date: 25/01/2016
- * Time: 14:42
+ * Date: 01/02/2016
+ * Time: 19:44
  * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
- * @copyright 2011-2014 Alex Alvarez G�rciga / Dandelion (http://www.thedandelionproject.com)
+ * @copyright 2011-2014 Alex Alvarez G?rciga / Dandelion (http://www.thedandelionproject.com)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @link      http://www.thedandelionproject.com
  */
 
 namespace Dandelion\Tools\CodeGenerator;
 
-use Dandelion\Tools\CodeGenerator\ICode;
-
-interface ICodeGenerator extends ICode
+interface ICode
 {
-    function InsertCode($code);
+    function getCode();
 }
