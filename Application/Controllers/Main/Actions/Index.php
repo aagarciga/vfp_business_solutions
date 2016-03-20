58e05673a95c2c805859b8d3f9d104eae933298a
Alex Alvarez Gárciga <aagarciga@gmail.com>
2016-03-19 22:11:19 -0400

    Added Menu From Settings by Company with default behavior in Main Controller.

--- Application/Controllers/Main/Actions/Index.php
+++ Application/Controllers/Main/Actions/Index.php
@@ -19,17 +19,6 @@ class Index extends Action {
      */
     public function Execute() {
         $this->Title = 'VFP Business Series';
-
-        $currentCompany = $this->Request->Application->getCompany($_SESSION['usercomp']);
-        if($currentCompany == ''){
-            // Default Company Configuration
-            $currentCompany = $this->Request->Application->getCompany();
-        }
-        $companyMenuEntries = $currentCompany->Menu->MenuEntry;
-        foreach ($companyMenuEntries as $menuEntry) {
-            error_log("Menu ENtry:  >>> ".$menuEntry['DisplayName']);
-        }
-
         $this->UserName = (!isset($_SESSION['username']))? 'Anonimous' : $_SESSION['username'];
         $this->FullFeatures = (!isset($_SESSION['fullFeatures']))? false : $_SESSION['fullFeatures'];
         $this->ShowFiancialDashboard = (!isset($_SESSION['showFiancialDashboard']))? false : $_SESSION['showFiancialDashboard'];
