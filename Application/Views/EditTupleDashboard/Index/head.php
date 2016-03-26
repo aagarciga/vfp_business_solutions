99216ec7cbcd30cf3f4790287f3c786aa35a6f5e
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-03-26 11:03:31 +0100

    Prepare body file Index view on EditTupleDashboard.

--- Application/Views/EditTupleDashboard/Index/head.php
+++ Application/Views/EditTupleDashboard/Index/head.php
@@ -11,6 +11,6 @@
 <link rel="stylesheet" href="<?php echo $View->PublicVendorContext('dropzone/css/dropzone.min.css'); ?>">
 <link rel="stylesheet" href="<?php echo $View->PublicVendorContext('jstree/themes/default/style.min.css'); ?>">
 <link rel="stylesheet" href="<?php echo $View->PublicVendorContext('responsive-tables/responsive-tables.css'); ?>">
-<link rel="stylesheet" href="<?php echo $View->StylesContext('EquipmentDashboard/index.min.css'); ?>">
+<link rel="stylesheet" href="<?php echo $View->StylesContext('EditTupleDashboard/index.min.css'); ?>">
 
 <link rel="stylesheet" href="<?php echo $View->PublicVendorContext('lightbox2-master/dist/css/lightbox.min.css')?>">
