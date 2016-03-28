3f801f41e14f67665a12c1798bc9d128ab3d7a51
Alex Alvarez Gárciga <aagarciga@gmail.com>
2016-03-28 00:15:40 -0400

    Main Navigation fetched from settings (Menu Entries used for this)

--- Application/Controllers/Main/Actions/Index.php
+++ Application/Controllers/Main/Actions/Index.php
@@ -22,8 +22,52 @@ class Index extends Action {
         $this->UserName = (!isset($_SESSION['username']))? 'Anonimous' : $_SESSION['username'];
         $this->FullFeatures = (!isset($_SESSION['fullFeatures']))? false : $_SESSION['fullFeatures'];
         $this->ShowFiancialDashboard = (!isset($_SESSION['showFiancialDashboard']))? false : $_SESSION['showFiancialDashboard'];
+        $this->HTMLMainPanelEntries = $this->getMainPanelEntries();
     }
 
+    /**
+     * Return the HTML LI collection of Main entries from
+     * configuration settings.xml ready for put inside an HTML UL element.
+     * @return string
+     */
+    private function getMainPanelEntries(){
+        $result = "";
+        $application = $this->Request->Application;
+        $currentCompany = $application->getCompany($_SESSION['usercomp']);
+        if($currentCompany == ''){
+            // Default Company Configuration
+            $currentCompany = $application->getCompany();
+        }
+        $companyMenuEntries = $currentCompany->Menu->MenuEntry;
+        foreach ($companyMenuEntries as $menuEntry) {
+
+            $entryHref = $this->view->Href($menuEntry['Controller'], $menuEntry['Action']);
+            $entryClassName = $menuEntry['ClassName'];
+            $entryDisplayName = $menuEntry['DisplayName'];
+
+            if (isset($menuEntry['IdName'])){
+                $entryIdName = $menuEntry['IdName'];
+                $entryHTML = "<li id=\"$entryIdName\"><a href=\"$entryHref\" class=\"\" ><span class=\"$entryClassName\"></span><span class=\"main-panel-caption\">$entryDisplayName</span> </a></li>\r";
+                // Visibility condition must correspond with session var name
+                // If menu entry contain a visibility condition
+                if(isset($menuEntry['VisibilityCondition'])){
+                    $conditionName = lcfirst($menuEntry['VisibilityCondition']);
+                    // Check visibility condition from session
+                    if(isset($_SESSION[$conditionName])){
+                        if($_SESSION[$conditionName]){
+                            // And include in result for visualization
+                            $result .= $entryHTML;
+                        }
+                    } else{
+                        error_log("$conditionName not found in SESSION: ". print_r($_SESSION,true));
+                    }
+                } else{ // If menu entry NOT contain a visibility condition, just include in result for visualization
+                    $result .= $entryHTML;
+                }
+            }
+        }
+        return $result;
+    }
 }
 
 ?>
