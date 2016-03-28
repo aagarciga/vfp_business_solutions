4c0437d4e320904be6c6f1c5e158e79f7d3b3d46
Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
2016-03-28 14:37:12 -0400

    Clear main.coffee on EditTupleDashboard.

--- Application/Views/EditTupleDashboard/Edit/head.php
+++ Application/Views/EditTupleDashboard/Edit/head.php
@@ -3,6 +3,10 @@
  * User: Victor
  * Date: 12/01/2016
  * Time: 15:01
+ * @author    Victor Luis Aguado Leon <victorluisaguadoleon@gmail.com>
+ * @copyright 2011-2014 Alex Alvarez G�rciga / Dandelion (http://www.thedandelionproject.com)
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.thedandelionproject.com
  */
 ?>
 
