7c0dd1e0eb222f8de4ccabc8f4f7133a7e5efe9d
Alex Alvarez Gárciga <aagarciga@gmail.com>
2016-04-03 19:02:16 -0400

    Cleaning Code.
    Reviewing all Application Copyrights.
    Adding Application Helpers Concept.
    	Adding Helpers class and View Helpers Class.

---
+++ Application/Helpers/Helpers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project:  VFP Business Series
+ * Copyright: 2014. VFP Business Solutions, LLC
+ */
+
+/**
+ * Class Utils for static helpers methods
+ */
+class Utils
+{
+    static function HelperMethod1(){
+        return "I'm a Helper method";
+    }
+}
