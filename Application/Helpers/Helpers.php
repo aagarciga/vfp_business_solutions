545942626743c66aeb878edbf2401c0ebe56b425
Alex Alvarez Gárciga <aagarciga@gmail.com>
2016-04-04 16:07:01 -0400

    Merge branch 'develop' into feature-equipment-add
    Conflicts:
    	Application/Libraries/Helpers/Builder/formEditTupleDashboard.php
    	Public/Scripts/EditTupleDashboard/main.min.js
    	Public/Scripts/EquipmentDashboard/main.min.js
    	Public/Scripts/main.min.js

---
+++ Application/Helpers/Helpers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project:  VFP Business Series
+ * Copyright: 2014. VFP Business Solutions, LLC
+ */
+
+/**
+ * Class Helpers for static helpers methods
+ */
+class Helpers
+{
+    static function HelperMethod1(){
+        return "I'm a Helper method";
+    }
+}
---
+++ Application/Helpers/ViewHelpers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project:  VFP Business Series
+ * Copyright: 2014. VFP Business Solutions, LLC
+ */
+
+/**
+ * Class View Helpers for static view helpers methods
+ */
+class ViewHelpers
+{
+    static function ViewHelperMethod1(){
+        return "I'm a View Helper method";
+    }
+}
